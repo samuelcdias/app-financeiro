84c5c25497375b9b0c803830389adba21b2ffe7a
XandyGomes <x3.computadores@hotmail.com>
2020-11-10 22:05:39 -0300

    10.11.2020

--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Login from '../views/login'
+import CadastroUsuario from '../views/cadastroUsuario'
 
 import 'bootswatch/dist/flatly/bootstrap.css'
+import '../custom.css'
 
 class App extends React.Component {
     render() {
         return(
             <div>
-                <Login />
+                <CadastroUsuario />
             </div>
         )
     }
